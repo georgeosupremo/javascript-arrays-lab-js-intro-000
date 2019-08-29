216176a522d0b962c07f510a5d1dd1fc3380b100
george.burton <george.burton@wework.com>
2019-08-29 22:43:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ kittens.unshift(name);
 return kittens
 }
 
-function destructivelyRemoveLastKitten(name){
+function destructivelyRemoveLastKitten(){
   kittens.pop();
   return kittens
 }
 
-function destructivelyRemoveFirstKitten(name){
+function destructivelyRemoveFirstKitten(){
   kittens.shift()
   return kittens
 }
 
+function appendKitten(name){
+  kittens.
+}
