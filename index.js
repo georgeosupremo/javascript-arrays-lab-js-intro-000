c16b45478ec971dff48b78b03e60cb3548b819b2
george.burton <george.burton@wework.com>
2019-08-29 22:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name){
 kittens.push(name);
 return kittens
 }
-function destructivelyAppendKitten(name){
+function destructivelyPrependKitten(name){
 kittens.unshift(name);
 return kittens
 }
